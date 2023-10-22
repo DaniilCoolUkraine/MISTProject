2a045e75465b1f33961d8d0f201fb3c74b9057cd
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-22 12:24:13 +0300

    di refactor, main widget rename and replace

---
+++ MIST_Project_Unity/Assets/Scripts/MonoInstallers/RequestHolderInstaller.cs
@@ -0,0 +1,13 @@
+﻿using MistProject.Requests;
+using Zenject;
+
+namespace MistProject.MonoInstallers
+{
+    public class RequestHolderInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<RequestHolder>().FromNewComponentOnNewGameObject().AsSingle();
+        }
+    }
+}
--- MIST_Project_Unity/Assets/Scripts/UI/MainWeatherWidgetRequestController.cs
+++ MIST_Project_Unity/Assets/Scripts/UI/MainWeatherWidgetRequestController.cs
@@ -2,21 +2,28 @@
 using MistProject.Requests;
 using MistProject.Requests.Response;
 using UnityEngine;
+using Zenject;
 
 namespace MistProject.UI
 {
-    public class Test : MonoBehaviour
+    public class MainWeatherWidgetRequestController : MonoBehaviour
     {
-        [SerializeField] private RequestHolder _requestHolder;
-
         private static readonly string API_LINK = Constants.GLOBAL_API_LINK + "current.json?" + "q=46.476608%2C30.707310" + "&" +
                                                   $"key={Constants.API_KEY}";
 
+        private RequestHolder _requestHolder;
+        
         private void Awake()
         {
             _requestHolder.SendGetRequest(API_LINK, null, ResponseActions);
         }
 
+        [Inject]
+        public void InjectDependencies(RequestHolder requestHolder)
+        {
+            _requestHolder = requestHolder;
+        }
+        
         private void ResponseActions(IResponseData responseData)
         {
             Debug.Log(responseData.GetText());
