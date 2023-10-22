c745e57e126fcd0cf2e214e68cf1fcfc05ea4915
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-22 11:39:16 +0300

    added constants and silly request holder test

---
+++ MIST_Project_Unity/Assets/Scripts/General/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MistProject.General
+{
+    public class Constants
+    {
+        public const string API_KEY = "759874ea2b5b4e649e0193658232110";
+
+        public const string GLOBAL_API_LINK = "https://api.weatherapi.com/v1/";
+    }
+}
---
+++ MIST_Project_Unity/Assets/Scripts/UI/Test.cs
@@ -0,0 +1,25 @@
+﻿using MistProject.General;
+using MistProject.Requests;
+using MistProject.Requests.Response;
+using UnityEngine;
+
+namespace MistProject.UI
+{
+    public class Test : MonoBehaviour
+    {
+        [SerializeField] private RequestHolder _requestHolder;
+
+        private static readonly string API_LINK = Constants.GLOBAL_API_LINK + "current.json?" + "q=46.476608%2C30.707310" + "&" +
+                                                  $"key={Constants.API_KEY}";
+
+        private void Awake()
+        {
+            _requestHolder.SendGetRequest(API_LINK, null, ResponseActions);
+        }
+
+        private void ResponseActions(IResponseData responseData)
+        {
+            Debug.Log(responseData.GetText());
+        }
+    }
+}
