641ad091e85e446a94e20b25e5793eb3bcb1e69e
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-24 19:46:18 +0300

    connected location to request

--- MIST_Project_Unity/Assets/Scripts/General/Constants.cs
+++ MIST_Project_Unity/Assets/Scripts/General/Constants.cs
@@ -5,6 +5,7 @@
         public const string API_KEY = "759874ea2b5b4e649e0193658232110";
 
         public const string GLOBAL_API_LINK = "https://api.weatherapi.com/v1/";
+        public const string COMMA_CODE = "%2C";
 
         public const float LOCATION_ACCURACY = 500;
         public const float LOCATION_UPDATE = 500;
--- MIST_Project_Unity/Assets/Scripts/UI/MainWeatherWidgetRequestController.cs
+++ MIST_Project_Unity/Assets/Scripts/UI/MainWeatherWidgetRequestController.cs
@@ -1,4 +1,5 @@
-﻿using MistProject.General;
+﻿using System.Globalization;
+using MistProject.General;
 using MistProject.Requests;
 using MistProject.Requests.Response;
 using MistProject.Utils;
@@ -9,17 +10,10 @@ namespace MistProject.UI
 {
     public class MainWeatherWidgetRequestController : MonoBehaviour
     {
-        private static readonly string API_LINK = Constants.GLOBAL_API_LINK + "current.json?" +
-                                                  "q=46.476608%2C30.707310" + "&" +
-                                                  $"key={Constants.API_KEY}";
+        private string _emptyApiLink = Constants.GLOBAL_API_LINK + "current.json?" + "q=&" + $"key={Constants.API_KEY}";
 
         private RequestHolder _requestHolder;
         private LocationUtils _locationUtils;
-
-        private void Awake()
-        {
-            _requestHolder.SendGetRequest(API_LINK, null, ResponseActions);
-        }
 
         [Inject]
         public void InjectDependencies(RequestHolder requestHolder, LocationUtils locationUtils)
@@ -34,7 +28,13 @@ namespace MistProject.UI
         private void LocationGetSuccessCallback(LocationInfo location)
         {
             _locationUtils.OnLocationGetError -= LocationGetError;
-            Debug.Log($"{location.latitude}, {location.longitude}");
+
+            var filledApiLink = _emptyApiLink.Insert(_emptyApiLink.IndexOf("q=") + 2,
+                $"{location.latitude.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}{Constants.COMMA_CODE}{location.longitude.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}");
+
+            Debug.Log($"<color=green>Sending to {filledApiLink}</color>");
+
+            _requestHolder.SendGetRequest(filledApiLink, null, ResponseActions);
         }
 
         private void LocationGetError(LocationErrors locationErrors)
