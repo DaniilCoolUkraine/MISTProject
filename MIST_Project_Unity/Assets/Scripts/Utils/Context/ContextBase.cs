ff6d7edaf52eb0cc72feab77d30af5fb91d57825
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-26 19:27:21 +0300

    added context manager

---
+++ MIST_Project_Unity/Assets/Scripts/Utils/Context/ContextBase.cs
@@ -0,0 +1,7 @@
+﻿namespace MistProject.Utils.Context
+{
+    public abstract class ContextBase
+    {
+        
+    }
+}
---
+++ MIST_Project_Unity/Assets/Scripts/Utils/Context/ContextManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MistProject.Utils.Context
+{
+    public class ContextManager
+    {
+        private static ContextManager _instance;
+        
+        public static ContextManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ContextManager();
+                }
+
+                return _instance;
+            }
+        }
+        
+        private Dictionary<Type, ContextBase> _contextBindings;
+
+        public void BindContext<T>(T context) where T : ContextBase
+        {
+            if (_contextBindings == null)
+            {
+                _contextBindings = new Dictionary<Type, ContextBase>();
+            }
+            _contextBindings[typeof(T)] = context;
+        }
+
+        public bool TryGetContext<T>(out T context) where T : ContextBase
+        {
+            context = null;
+            if (_contextBindings.TryGetValue(typeof(T), out var getContext))
+            {
+                context = (T) getContext;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveContext<T>() where T : ContextBase
+        {
+            throw new NotImplementedException($"{nameof(RemoveContext)} isn't implemented");
+        }
+    }
+}
