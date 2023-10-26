100f011515e7e0614a24975247adcc46680548db
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-26 19:11:02 +0300

    added global settings and global settings installer

---
+++ MIST_Project_Unity/Assets/Scripts/Config/GlobalSettingsSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MistProject.Config
+{
+    [CreateAssetMenu(fileName = "GlobalSettings", menuName = "Config/GlobalSettings", order = 0)]
+    public class GlobalSettingsSO : ScriptableObject
+    {
+        [field:SerializeField] public bool UseCelsius { get; private set; }
+    }
+}
---
+++ MIST_Project_Unity/Assets/Scripts/MonoInstallers/GlobalSettingsInstaller.cs
@@ -0,0 +1,17 @@
+﻿using MistProject.Config;
+using UnityEngine;
+using Zenject;
+
+namespace MistProject.MonoInstallers
+{
+    [CreateAssetMenu(fileName = "GlobalSettingsInstaller", menuName = "Config/GlobalSettingsInstaller", order = 0)]
+    public class GlobalSettingsInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private GlobalSettingsSO _globalSettings;
+        
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<GlobalSettingsSO>().FromInstance(_globalSettings).AsSingle();
+        }
+    }
+}
