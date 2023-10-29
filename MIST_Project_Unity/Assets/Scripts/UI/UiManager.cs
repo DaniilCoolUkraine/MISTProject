fd93fd73d22aa05b9a9097cf0e7dfc0664f056ad
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-10-29 10:21:00 +0200

    connected forecast manager to request and ui

--- MIST_Project_Unity/Assets/Scripts/UI/UiManager.cs
+++ MIST_Project_Unity/Assets/Scripts/UI/UiManager.cs
@@ -1,4 +1,5 @@
-﻿using MistProject.UI.MainWeather;
+﻿using MistProject.UI.Forecast;
+using MistProject.UI.MainWeather;
 using MistProject.Utils.Context;
 using UnityEngine;
 
@@ -9,11 +10,15 @@ namespace MistProject.UI
         [SerializeField] private MainWeatherWidgetController _mainWeatherWidgetController;
         [SerializeField] private MainWeatherWidgetRequestController _mainWeatherRequest;
         [SerializeField] private AdditionalWeatherInfoController _additionalWeatherInfoController;
+        [SerializeField] private ForecastRequestController _forecastRequestController;
+        [SerializeField] private ShortForecastManager _shortForecastManager;
 
         private void OnEnable()
         {
             _mainWeatherRequest.OnRequestSuccess += _mainWeatherWidgetController.SetTexts;
             _mainWeatherRequest.OnImageLoaded += _mainWeatherWidgetController.SetWeatherIcon;
+            _forecastRequestController.OnRequestSuccess += _shortForecastManager.UpdateValues;
+
             ContextManager.Instance.OnContextUpdated += _additionalWeatherInfoController.UpdateValues;
         }
 
@@ -21,6 +26,8 @@ namespace MistProject.UI
         {
             _mainWeatherRequest.OnRequestSuccess -= _mainWeatherWidgetController.SetTexts;
             _mainWeatherRequest.OnImageLoaded -= _mainWeatherWidgetController.SetWeatherIcon;
+            _forecastRequestController.OnRequestSuccess -= _shortForecastManager.UpdateValues;
+
             ContextManager.Instance.OnContextUpdated -= _additionalWeatherInfoController.UpdateValues;
         }
     }
